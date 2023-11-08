e7512f45960a57dd0b63a59949c93df39e8771a4
davi <davi@intrafatec.lc>
2023-11-08 17:46:29 -0300

    .

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
     require_once("includes/banco.php");
+    $buscaProdutos = $bd->query("select * from vwProdutos");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,9 +17,46 @@
 </head>
 <body>
     <?php include("includes/header.php");?>
-    <section class="introSite">
-        <div class="banner">
+    <div class="bannerIntro">
             <img src="logo/svg/bannerIndex.svg" alt="banner">
+    </div>
+    <section class="introIndex">
+        <div class="container"> 
+            <div class="recomendBar">
+                <h2>RECOMENDADO</h2>
+            </div>
+            <div class="recomendadosProdutos">
+                <div class="produtos prodRec">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/IMG_Academy_Logo.svg/1200px-IMG_Academy_Logo.svg.png" alt="" width="268" height="162">
+                    <p class="nome">{$prod->nome}</p>
+                    <p class="preco">{$prod->precoAvista}</p>
+                    <p class="avisoPix">À vista no PIX</p>
+                </div>
+                <div class="produtos prodRec">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/IMG_Academy_Logo.svg/1200px-IMG_Academy_Logo.svg.png" alt="" width="268" height="162">
+                    <p class="nome">{$prod->nome}</p>
+                    <p class="preco">{$prod->precoAvista}</p>
+                    <p class="avisoPix">À vista no PIX</p>
+                </div>
+                <div class="produtos prodRec">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/IMG_Academy_Logo.svg/1200px-IMG_Academy_Logo.svg.png" alt="" width="268" height="162">
+                    <p class="nome">{$prod->nome}</p>
+                    <p class="preco">{$prod->precoAvista}</p>
+                    <p class="avisoPix">À vista no PIX</p>
+                </div>
+                <div class="produtos prodRec">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/IMG_Academy_Logo.svg/1200px-IMG_Academy_Logo.svg.png" alt="" width="268" height="162">
+                    <p class="nome">{$prod->nome}</p>
+                    <p class="preco">{$prod->precoAvista}</p>
+                    <p class="avisoPix">À vista no PIX</p>
+                </div>
+                <div class="produtos prodRec">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/IMG_Academy_Logo.svg/1200px-IMG_Academy_Logo.svg.png" alt="" width="268" height="162">
+                    <p class="nome">{$prod->nome}</p>
+                    <p class="preco">{$prod->precoAvista}</p>
+                    <p class="avisoPix">À vista no PIX</p>
+                </div>
+            </div>
         </div>
     </section>
 </body>
