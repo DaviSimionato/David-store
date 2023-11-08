b992f68d84be134748054a0c0a1055783e5d7c69
davi <davi@intrafatec.lc>
2023-11-08 16:59:43 -0300

    .

--- index.php
+++ index.php
@@ -15,23 +15,6 @@
     <title>David'store</title>
 </head>
 <body>
-    <header>
-        <div class="header">
-            <span class="material-symbols-outlined menuLateral">menu</span>
-            <img src="logo/svg/logo-no-background.svg" alt="Logo" height="20">
-            <div class="barraPesquisa">
-                <input type="text" name="pesquisa" class="pesqBar">
-                <span class="material-symbols-outlined">search</span>
-            </div>
-            <div class="entrarOuCad">
-                <span class="material-symbols-outlined">account_circle</span>
-                <p><strong><a href="#">Entrar</a></strong> ou <strong><a href="#">Cadastre-se</a></strong></p>
-            </div>
-            <div class="linksUser">
-                <a href="#"><span class="material-symbols-outlined fav" title="Favoritos">favorite</span></a>
-                <a href="#"><span class="material-symbols-outlined" title="Carrinho">shopping_cart</span></a>
-            </div>
-        </div>
-    </header>
+    <?php include("includes/header.php");?>
 </body>
 </html>
