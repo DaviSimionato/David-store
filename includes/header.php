a4ea29c50e7b10b6cd737fc1461d45587e68146c
davi <davi@intrafatec.lc>
2023-11-08 17:48:42 -0300

    .

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<header>
+        <div class="header">
+            <span class="material-symbols-outlined menuLateral">menu</span>
+            <img src="logo/svg/logo-no-background.svg" alt="Logo" height="20">
+            <div class="barraPesquisa">
+                <input type="text" name="pesquisa" class="pesqBar">
+                <a href="#"><span class="material-symbols-outlined" title="Pesquisar">search</span></a>
+            </div>
+            <div class="entrarOuCad">
+                <span class="material-symbols-outlined">account_circle</span>
+                <p><strong><a href="#">Entrar</a></strong> ou <strong><a href="#">Cadastre-se</a></strong></p>
+            </div>
+            <div class="linksUser">
+                <a href="#"><span class="material-symbols-outlined fav" title="Favoritos">favorite</span></a>
+                <a href="#"><span class="material-symbols-outlined" title="Carrinho">shopping_cart</span></a>
+            </div>
+        </div>
+    </header>
